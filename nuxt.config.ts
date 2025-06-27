b9d6d2c5a33d71323746f17f6d4181929f8c2a40
Mert Demirbaş <mertdemirbas58@gmail.com>
2025-06-27 11:54:28 +0300

    chore: configure Nuxt & Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,1 +1,17 @@
-export default {}
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  ssr: true,
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  css: [
+    '@/assets/css/tailwind.css'
+  ]
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,15 @@
-export default {}
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: [
+    './components/**/*.{vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
